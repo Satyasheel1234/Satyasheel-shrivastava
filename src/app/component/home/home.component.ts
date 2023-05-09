c04e2876509d0a3a99e4b414407bbea667a7dc77
sam.shrivastava11@gmail.com <satyasheel.s@apar.com>
2023-05-09 12:00:24 +0200

    remove email

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,27 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { ContactFormService } from 'src/app/contact-form.service';
-
+import Swiper from 'swiper';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-  name: any;
-  email: any;
-  message: any;
-  constructor(private contactFormService: ContactFormService) { }
+
+  constructor() { }
 
   ngOnInit(): void {
     
-  }
-  onSubmit() {
-    this.contactFormService.sendEmail(this.name, this.email, this.message)
-      .subscribe(() => {
-        console.log('Email sent successfully');
-      }, () => {
-        console.log('Email failed to send');
-      });
   }
   
 
