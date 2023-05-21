3f6af34be9132a2812c4f3fe1cccec745a4e03b7
sam.shrivastava11@gmail.com <satyasheel.s@apar.com>
2023-05-21 17:47:55 +0200

    added email

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import Swiper from 'swiper';
 @Component({
@@ -6,11 +7,35 @@ import Swiper from 'swiper';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  name: any;
+  email: any;
+  message: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     
+  }
+  
+  submitForm(): void {
+    const data = {
+      name: this.name,
+      email: this.email,
+      message: this.message
+    };
+
+    this.http.post('http://localhost:3000/contact', data).subscribe(
+      response => {
+        console.log(response);
+        // Reset the form
+        this.name = '';
+        this.email = '';
+        this.message = '';
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
   
 
